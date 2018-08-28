e3c6ea0b12ce1a3734c85653858149029e60a079
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-08-28 14:55:37 -0300

    Criando o button na main activity para ir na activity do toggle

--- app/src/main/java/com/example/iarlenaquiles/estudoandroid/MainActivity.java
+++ app/src/main/java/com/example/iarlenaquiles/estudoandroid/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.iarlenaquiles.estudoandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButtonToggle();
+    }
+
+    public void addListenerOnButtonToggle() {
+        Button btnToggle = (Button) findViewById(R.id.btnToggle);
+
+        btnToggle.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, ToggleActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
