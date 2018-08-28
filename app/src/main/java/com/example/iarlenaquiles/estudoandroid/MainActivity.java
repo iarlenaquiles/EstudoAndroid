5818876ad2c9a007965b868faf6ee8d07499809e
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-08-28 15:06:59 -0300

    Criando o button para chamar a edittextactivity

--- app/src/main/java/com/example/iarlenaquiles/estudoandroid/MainActivity.java
+++ app/src/main/java/com/example/iarlenaquiles/estudoandroid/MainActivity.java
@@ -5,24 +5,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnToggle;
+    private Button btnEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addListenerOnButtonToggle();
+        addListenerOnButtonEditText();
     }
 
     public void addListenerOnButtonToggle() {
-        Button btnToggle = (Button) findViewById(R.id.btnToggle);
+        btnToggle = (Button) findViewById(R.id.btnToggle);
 
         btnToggle.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent it = new Intent(MainActivity.this, ToggleActivity.class);
+                startActivity(it);
+            }
+        });
+    }
+
+    public void addListenerOnButtonEditText() {
+        btnEditText = (Button) findViewById(R.id.btnEditText);
+
+        btnEditText.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, EditTextActivity.class);
                 startActivity(it);
             }
         });
